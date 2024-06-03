0c10cafc1b10bd5cf0ab9e158d358287d36c1afa
xyc <1598027117@qq.com>
2024-06-03 17:03:31 +0800

    debug

--- BigDataLab3/src/main/java/EdgeProcess.java
+++ BigDataLab3/src/main/java/EdgeProcess.java
@@ -6,16 +6,17 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class EdgeProcess {
 
-
     public static class EdgeProcessMapper extends Mapper<Object, Text, Text, Text> {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] websiteId = value.toString().split(", ");
             String edge = null;
             String val = "2";
-            if (websiteId[0].compareTo(websiteId[1]) < 0) {
+            int id1 = Integer.parseInt(websiteId[0]);
+            int id2 = Integer.parseInt(websiteId[1]);
+            if (id1 < id2) {
                 edge = websiteId[0] + " " + websiteId[1];
-            } else if (websiteId[0].compareTo(websiteId[1]) > 0) {
+            } else if (id2 < id1) {
                 edge = websiteId[1] + " " + websiteId[0];
                 val = "1";
             }
@@ -25,7 +26,7 @@ public class EdgeProcess {
 
     public static class EdgeProcessReducer extends Reducer<Text, Text, Text, Text> {
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-//            int cnt = 0;
+            // int cnt = 0;
             int isDouble = 0;
             for (Text val : values) {
                 int valInt = Integer.parseInt(val.toString());
