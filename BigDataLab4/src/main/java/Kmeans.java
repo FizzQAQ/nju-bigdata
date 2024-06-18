9627da946f0db285c742018b642554f08a69df56
fizzqaq <fizzqaq@qq.com>
2024-06-18 19:46:25 +0800

    Lab4

--- BigDataLab4/src/main/java/Kmeans.java
+++ BigDataLab4/src/main/java/Kmeans.java
@@ -1,7 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
 import java.util.StringTokenizer;
+import java.io.BufferedReader;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
@@ -9,6 +11,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+
 import org.apache.hadoop.fs.*;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -23,20 +26,23 @@ public class Kmeans {
         
         private List<double[]> clusters = new ArrayList<double[]>();
         protected void setup(Context context) throws IOException, InterruptedException {
-            Configuration conf = context.getConfiguration();
-            Path path = new Path(conf.get("centroidsPath"));
-            BufferedReader br = new BufferedReader(new FileReader(String.valueOf(path)));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] temp = line.split(",: ");
-                double[] cluster = new double[temp.length-1];
-                for (int i = 0; i < temp.length-1; i++) {
-                    cluster[i] = Double.parseDouble(temp[i+1]);
-                }
-                clusters.add(cluster);
+        Configuration conf = context.getConfiguration();
+        Path centroidsPath = new Path(conf.get("centroidsPath"));
+        FileSystem fs = FileSystem.get(conf);
+        FSDataInputStream fsDataInputStream = fs.open(centroidsPath);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fsDataInputStream));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] temp = line.split(",: ");
+            double[] cluster = new double[temp.length-1];
+            for (int i = 0; i < temp.length-1; i++) {
+                cluster[i] = Double.parseDouble(temp[i+1]);
             }
-            br.close();
+            clusters.add(cluster);
         }
+        br.close();
+        }
+
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] temp = value.toString().split(",: ");
             double[] data = new double[temp.length-1];
